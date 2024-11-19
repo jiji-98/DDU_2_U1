e32b357c5d348b8704fb36d934d536b8a94390f2
jiji-98 <rebecca.jarljohansson@gmail.com>
2024-11-19 13:38:21 +0100

    Flyttade material
    Jag flyttade mitt material + index.js + index.html till direkt i repository mappen från mappen website. Anledningen är för att minska antalet steg när jag skapar mina sökvägar senare

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DDU_2_U1
- 
+
+Sökväg till GitHub
+https://github.com/jiji-98/DDU_2_U1
+
--- website/index.js
+++
@@ -1,6 +0,0 @@
-// Recommended: All functions declared here
-
-// Recommended: constants with references to existing HTML-elements
-
-// Recommended: Ask for the city name and then the rest of the code
-

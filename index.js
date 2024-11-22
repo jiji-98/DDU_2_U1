22e1a390584a18e241a729aa024987942731980a
jiji-98 <rebecca.jarljohansson@gmail.com>
2024-11-22 10:09:37 +0100

    Funktion för prompt
    Det första jag gör är att jag skapar en ny variabel, vars värde ska vara returvärdet av en funktion.
    
    Nu har jag skapat en ny funktion som ska ha en parameter (cityName) vars värde är cityFromUser (vårt prompt värde). Inne i funktionen så använder jag mig av en for of loop för att hämta objekten är arrayen cities. Sedan så jämför jag om en av objekten i arrayen vars värde till egenskapen .name matchar parameter värdet.
    Och om det matchar så returnerar jag värdet till den nya variabeln på rad 6.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const cityFromUser = prompt("Type in a city please")
 
 // <----- hitta rätt stad i arrayen ---->
 
+const foundCity = findCity(cityFromUser);
+
+function findCity(cityName) {
+    for (city of cities) {
+        if (city.name === cityName) {
+            return city;
+
+        }
+    }
+}
+

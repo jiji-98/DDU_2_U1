65ec9bd96653c51f8583b5205f5feb363fc59be6
jiji-98 <rebecca.jarljohansson@gmail.com>
2024-11-25 19:31:11 +0100

    Ändringar
    Jag har flyttat upp const cityFromUser och tagit bort mina kommentarer. Jag har också valt att deklarera city på rad 6 istället för att bara skriva city of cities. På rad 11 ersatte jag det som tidigare stod cityFromUser till dess parameter cityName, som det ska stå i en funktion. På rad 27 & 28 så valde jag att ta bort variablerna på längd värdet och istället skriva värdet direkt i for looparna jag använde härnäst.

--- README.md
+++
@@ -1,5 +0,0 @@
-# DDU_2_U1
-
-Sökväg till GitHub
-https://github.com/jiji-98/DDU_2_U1
-
--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-
 const cityFromUser = prompt("Type in a city please")
-
-// <----- hitta rätt stad i arrayen ---->
 
 const foundCity = findCity(cityFromUser);
 
 function findCity(cityName) {
-    for (city of cities) {
+    for (let city of cities) {
         if (city.name === cityName) {
             return city;
         }
         if (city.name !== cityName) {
-            document.querySelector("h2").textContent = `${cityFromUser} finns inte i databasen`;
+            document.querySelector("h2").textContent = `${cityName} finns inte i databasen`;
             document.querySelector("title").textContent = `Not Found`;
             document.querySelector("h3").textContent = ``;
-
         }
     }
 }
-
 
 let distanceTable = tableOfDistance();
 
 function tableOfDistance() {
     let tabel = document.querySelector("#table");
-    const rows = cities.length;
-    const columns = cities.length + 1; // En extra för header-raden
 
         }
     }
